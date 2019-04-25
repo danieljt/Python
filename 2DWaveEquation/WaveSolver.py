4f3eb1131f77d03a8d6f704fe95fddb8e8f421fe
Daniel James Tarplett <danieljamestarplett@hotmail.com>
2019-04-25 19:09:21 +0200

    Renamed folders

---
+++ 2DWaveEquation/WaveSolver.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Wave2D:
+    
+    def __init__(self, x0, x1, nx, y0, y1, ny):
+        self.x0 = x0; self.x1 = x1; self.nx = nx;
+        self.y0 = y0; self.y1 = y1; self.ny = ny;
+
+    def set_initial_condition(self, I, V):      
+        self.I = I
+        self.V = V
+
+    def 
+
+    
+
+        
+        
